a5cc6dd7e5cf2d778730af4923b11d5df067e234
CPMuise <Christian.Muise@dal.ca>
2016-11-22 23:17:57 -0400

    Update bakery.cpp
    Began fleshing-out main()

--- bakery.cpp
+++ bakery.cpp
@@ -12,27 +12,69 @@
 #include <deque>
 #include <list>
 
+#include "bakery.h"
+#include "customer.h"
+#include "food.h"
+#include "supplier.h"
+#include "tallySheet.h"
+
 using namespace std;
 
-#include "bakery.h"
-
-// Include all necessary standard headers...
-// ... and custom headers
-// Define Constants as required
-
-/* CLASS DECLARATION */
-// Don't actually; declare classes and define their functions in files to be included above
+const int SIM_DURATION  = 16*60; // number of minutes the simulation runs for
 
 int main(void)
 {
-  // Initialize variables
-  
-  // Read Input files
-  
-  // Open-up shop
-  // Iterate minute-by-minute through day
+	// Initialize variables
+	ifstream custIn("input.txt");
+	ifstream paramIn("input2.txt");
+	ofstream foodRptOut("output1.txt");
+	ofstream custRptOut("output2.txt");
+	ofstream smryRptOut("output3.txt");
+	
+	// Parameters from input files
+	double lambda;
+	double mu;
+	double cakeKitPrice;
+	double cakePrice;
+	double croissantKitPrice;
+	double croissantprice;
+	int totalCustomers;
+ 	
+ 	// Gather parameters from paramIn
+ 	paramIn >> lambda;
+	paramIn >> mu;
+	paramIn >> cakeKitPrice;
+	paramIn >> cakePrice;
+	paramIn >> croissantKitPrice;
+	paramIn >> croissantprice;
+	
+	// Gather parameters from custIn	
+	custIn >> totalCustomers;
+	
+	// Create Master Lists
+	list<customer> custMaster;
+	list<foodItem> foodMaster;
+	
+	// Set up scenario
+	bakery   OldBedford(cakePrice, croissantPrice, custMaster, foodMaster);
+	supplier kitSupplier(cakeKitPrice, croissantKitPrice, custMaster, foodMaster);
+ 	
+  	for(int t = 0; t <= SIM_DURATION; t++){
+  		
+		  // Check up on orders
+  		
+  		
+  	}
+ 	// Open-up shop
+ 	// Iterate minute-by-minute through day
     // Check input files
     // React appropriately
   
-  // Close-up Shop  
+	// Close-up Shop
+	custIn.close();
+	paramIn.close();
+	foodRptOut.close();
+	CustRptOut.close();
+	SmryRptOut.close();
+	return 0
 }
